c838ef960bf2e1a75e3c29a2d43ab89349a1d0f2
xabibullayevuser <xusniddinxabibullayev25@gmail.com>
2024-05-02 22:50:55 +0500

    ERP

---
+++ app.js
@@ -0,0 +1,150 @@
+//Edabit -- 1-masala
+// function sum(son1, son2) {
+//     return son1 + son2;
+// }
+
+// let result = sum(3, 2);
+// console.log(result);
+
+//2-masala
+
+// function tosekund(minute) {
+//     return minute * 60;
+//   }
+
+//   let sekund10Minute = tosekund(10);
+//   console.log(sekund10Minute);
+
+//3-masala
+
+// function nextInteger(int) {
+//   return int + 1;
+// }
+  
+// const nextNumber = nextInteger(9);
+// console.log(nextNumber);
+
+//4-masala
+// function uchburchakYuzi(asos, balandlik) {
+   
+//     return (asos * balandlik) / 2;
+//   }
+  
+//   let yuzi = uchburchakYuzi(7, 4);
+//   console.log(yuzi);
+
+//5-masala
+// function ageToDays(age) {
+//     return age * 365;
+//   }
+  
+//   let ageDays = ageToDays(65);
+//   console.log(ageDays);
+
+//6-masala
+// function cube(son) {
+//     return son * son * son;
+// }
+
+//  console.log(cube(10));
+
+//7-masala
+// function firstElement(arr) {
+//     return arr[0];
+//   }
+
+//   let numbers = [1, 2, 3];
+//   let firstNumber = firstElement(numbers);
+//   console.log(firstNumber);
+
+//8-masala
+// function product(x, y) {
+//     return x * y;
+//   }
+  
+//   let result = product(230, 10);
+//   console.log(result); 
+
+//9-masala
+
+// function hourToSekunds(clock) {
+//     if (!isNumber(clock) || clock < 0);
+   
+//     const seconds = clock * 60 * 60;
+//     return seconds;
+//   }
+  
+//   function isNumber(value) {
+//     return typeof value === 'number';
+//   }
+  
+//   try {
+//     const clockValue = 2;
+//     const seconds = hourToSekunds(clockValue);
+//     console.log(`${clockValue}  ${seconds} `);
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+  
+
+//10-masala
+// function uchunchiTomon(tomon1, tomon2) {
+//     if (tomon1 <= 0 || tomon2 <= 0) {
+//       throw new Error("Burchakning tomonlari juft son bulishi kerak");
+//     }
+  
+//     const umumiyUzunlik = tomon1 + tomon2;
+  
+//     const engUznTomon = umumiyUzunlik - 1;
+  
+//     return engUznTomon;
+//   }
+  
+//   try {
+//     console.log(uchunchiTomon(8, 10)); 
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+  
+//11-masala
+// function qoldiq(son1, son2) {
+//     return son1 % son2;
+//   }
+  
+//   console.log(qoldiq(1, 3));
+
+//12-masala
+// function turtBurchakYuzi(boyi, eni) {
+//     let yuzi = boyi * eni;
+//     return yuzi;
+//   }
+
+//13-masala
+// function stringQoshish(a) {
+//     return "FrontEnd " + a;
+//   }
+  
+//   const inputString = "better than BackEnd";
+//   const resultString = stringQoshish(inputString);
+//   console.log(resultString);
+
+//14-masala
+// function kvadrat(son) {
+//     return son * son;
+//   }
+  
+//   result = kvadrat(5)
+//   console.log(result);
+
+//15-masala
+// function noldan(number) {
+//     return number <= 0;
+//   }
+  
+//   console.log(noldan(5));
+
+  
+  
+  
+
+  
